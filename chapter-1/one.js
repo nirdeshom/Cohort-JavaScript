2e40daddef8ad73e1f9fcce527c41393c09ca023
Nirdesh Kumar <nirdeshkumarom@gmail.com>
2025-05-23 08:27:17 +0530

    create factory

---
+++ chapter-1/one.js
@@ -0,0 +1,18 @@
+function add(num1, num2){
+    console.log(num1 + num2);
+}
+
+function bringBrush(itne){
+    console.log(`le aaya bhai ${itne} brush`)
+}
+
+function multipleValue(x,y,z){
+    return x + y + z;
+}
+
+let result = multipleValue(2,2,2);
+console.log(result);
+console.log(multipleValue(3,4,5));
+
+bringBrush(6)
+add(2,3);
