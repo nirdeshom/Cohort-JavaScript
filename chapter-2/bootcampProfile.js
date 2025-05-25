a844e89f4240705d97af6a08a0d4aa3ee4ca7482
Nirdesh Kumar <nirdeshkumarom@gmail.com>
2025-05-25 20:59:44 +0530

    data type

---
+++ chapter-2/bootcampProfile.js
@@ -0,0 +1,31 @@
+// String - "", '', ``
+let name = "Nirdesh"
+
+// Number - 22, 22.43
+let age = 22;
+
+// Boolean - true, false
+let isPaid = true;
+
+let favoriteClass = null;
+let hometown = undefined;
+let branch;
+
+console.log(typeof null);
+console.log(typeof (null));
+console.log(typeof (branch));
+
+// Array 
+let skills = ["html", "css", "JavaScript", "java", "node.js"];
+
+// Object
+let studentProfile = {
+    name: "Nirdesh Kumar",
+    age: 22,
+    branch: "IT",
+    course: "Web Dev Cohort Live 1.0",
+    completed: ["html", "css"],
+    favoriteClass: null,
+}
+
+console.log(typeof studentProfile);  // typeof object
