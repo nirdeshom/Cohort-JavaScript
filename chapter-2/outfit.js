74a14a0d87ff9a73984a526a3697bf9bd5e9c68e
Nirdesh Kumar <nirdeshkumarom@gmail.com>
2025-05-30 09:32:35 +0530

    weather

---
+++ chapter-2/outfit.js
@@ -0,0 +1,13 @@
+// server - cloudy, rainy, sunny 
+
+let weather = "sunny";
+
+if (weather === "rainy") {
+    console.log("it's rainy umbrella le jana bhai");
+}
+else if (weather === "cloudy") {
+    console.log("Jacket lekar jana bhai");
+}
+else {
+    console.log("bhai mast ho ke jao");
+}
