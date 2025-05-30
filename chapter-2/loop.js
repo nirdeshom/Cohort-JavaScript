5107d7adea2740de7065fa7c60bf961c08e3901a
Nirdesh Kumar <nirdeshkumarom@gmail.com>
2025-05-30 09:31:09 +0530

    loop

---
+++ chapter-2/loop.js
@@ -0,0 +1,10 @@
+let fruits = ["Apple", "Banana", "Mango", "Orange"];
+
+// loop iteration 
+// variable lekar aao 
+// limit btao 
+// incre / decre
+
+for(let h = 0; h < fruits.length; h++){
+    console.log(`fruits at index ${h} : ${fruits[h]}`);
+}
