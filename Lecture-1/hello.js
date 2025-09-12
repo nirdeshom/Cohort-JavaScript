72994d414d4a1ff0e6dd15284208e893ae4cadf4
Nirdesh Kumar <nirdeshkumarom@gmail.com>
2025-09-12 15:20:39 +0530

    create a factory

---
+++ Lecture-1/hello.js
@@ -0,0 +1,22 @@
+// console.log("Welcome to Web Dev Cohort Live 1.0...");
+ 
+function printChai(){
+    console.log("Hello Chai Aur Code");
+}
+
+function bringBrush(numberOfBrush){
+    // console.log("Hmaji, le aaya brush");
+    console.log(`Hmaji, le aaya brush ${numberOfBrush}`);
+}
+
+function addTwo(num1, num2){
+    return num1 + num2;
+}
+
+printChai();
+bringBrush(4);
+
+let result = addTwo(2, 2);
+console.log(result);
+
+console.log(addTwo(5, 5));
